9ef53493bdb7987ce9f0813e2ed5e3502656f9af
Nico Franke <nico.franke01@gmail.com>
2020-10-07 14:23:43 +0200

    :tada: Initial commit

---
+++ src/main/java/de/pinpoint/server/PinpointServerApplication.java
@@ -0,0 +1,13 @@
+package de.pinpoint.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinpointServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinpointServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/pinpoint/server/PinpointServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pinpoint.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinpointServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
